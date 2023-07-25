80c38e5bc187e9b20734c55ef1cf4bf1f3bafea1
NilPointerError <gond_123@163.com>
2023-07-25 17:11:14 +0800

    Commit 模块化.html

---
+++ es6/js/hello.js
@@ -0,0 +1,16 @@
+// export const util = {
+//     sum(a,b) {
+//         return a+b;
+//     }
+// }
+
+// export {util}
+
+export default {
+    sum(a,b) {
+        return a+b;
+    }
+}
+
+
+//export 不仅可以导出对象，一切js变量都可以导出
---
+++ es6/js/main.js
@@ -0,0 +1,7 @@
+import abc from "./hello.js"
+import {name,age,add} from "./user.js"
+
+
+abc.sum(1,2);
+console(name);
+add(1,3);
