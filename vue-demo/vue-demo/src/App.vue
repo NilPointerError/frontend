6c07dbdeaf6d8cc9e9957edab365a4b546110646
NilPointerError <gond_123@163.com>
2023-07-26 16:06:26 +0800

    Add Hello.vue

--- vue-demo/vue-demo/src/App.vue
+++ vue-demo/vue-demo/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <router-link to="/hello">去hello</router-link>
+    <router-link to="/">去首页</router-link>
+    <!-- 路由视图 -->
     <router-view/>
   </div>
 </template>
---
+++ vue-demo/vue-demo/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>你好,hello,{{name}}</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name:"张三"
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
